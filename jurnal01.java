22942927d14c0faa46277c133c54af382b07e602
Dzaky03 <muhammaddzaky2017@gmail.com>
2023-09-28 19:20:04 +0700

    jurnal1

---
+++ jurnal01.java
@@ -0,0 +1,15 @@
+
+public class jurnal01 {
+    public static void main(String[] args) {
+        // Mendefinisikan variabel untuk NIM, Nama, dan Kelas
+        String nim = "6706223022";
+        String nama = "Muhammad Dzaky";
+        String kelas = "46_05";
+
+        // Menampilkan informasi Mahasiswa
+        System.out.println("NIM: " + nim);
+        System.out.println("Nama: " + nama);
+        System.out.println("Kelas: " + kelas);
+    }
+}
+
